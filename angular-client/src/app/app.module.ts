11214568afb5f04a157f45307eea2ba07c20ddba
vasanths <sreeranga>
2019-02-23 16:57:19 -0500

    Implement Reactive Forms for login page

--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule} from '@angular/forms';
+import { ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +16,7 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule
   ],
--- angular-client/src/app/login/login.component.ts
+++ angular-client/src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,28 +10,44 @@ import { Observable } from 'rxjs';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  model: any = {};
+  loginForm: FormGroup;
+  submitted = false;
+  loading = false;
 
   constructor(
-    private route: ActivatedRoute,
+    private formBuilder: FormBuilder,
     private router: Router,
     private http: HttpClient
 
   ) { }
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+  });
+
+
     sessionStorage.setItem('token', '');
   }
 
-  login() {
+  // convenience getter for easy access to form fields
+  get f() { return this.loginForm.controls; }
+
+  onSubmit() {
+
+    this.submitted = true;
+    // stop here if form is invalid
+  if (this.loginForm.invalid) {
+    return;
+}
     const url = 'http://localhost:8080/login';
     this.http.post<Observable<boolean>>(url, {
-      username: this.model.username,
-    password: this.model.password })
+      username: this.f.username.value,
+    password: this.f.password.value })
     .subscribe(isValid => {
       if (isValid) {
-        sessionStorage.setItem('token', btoa(this.model.username + ':' + this.model.password));
+        sessionStorage.setItem('token', btoa(this.f.username.value + ':' + this.f.password.value));
         this.router.navigate(['']);
       } else {
         alert('Authentication failed.');
